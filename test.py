6b1ef445dd6461bb366a992dfba6a98f6a104cbe
petbell <35951575+petbell@users.noreply.github.com>
2025-12-26 18:34:31 +0100

    test

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ async def get_product(item_id : int):
     for product in products:
         if product.id == item_id:
             return {"products" : product }    
-        return {"products" : "product not found" }
+        return {"products" : "product not found or missing" }
     
 # post a product
 @app.post("/products")
